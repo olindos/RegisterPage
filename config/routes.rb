5191301d9a1a95ea9d6904b9bcdd7e9b365ce04f
olindos <yason4ik87@gmail.com>
2018-10-25 20:38:51 +0300

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'completeregister/home'
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, path: '', path_names: { sign_up: 'register' }
+
+  authenticated :user do
+    root to: 'completeregister#home'
+  end
 end
