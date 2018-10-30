25b633ecb9c2b88d791c83947526afe47fb081c5
olindos <yason4ik87@gmail.com>
2018-10-30 13:58:31 +0200

    Change validator disable button

--- app/assets/javascripts/disable_button.js
+++ app/assets/javascripts/disable_button.js
@@ -1,14 +1,11 @@
-function checkParams() {
-  var email = $('#user_email').val();
-  var nickname = $('#user_nickname').val();
-  var first_name = $('#user_first_name').val();
-  var last_name = $('#user_last_name').val();
-  var password = $('#user_password').val();
-  
-  if(nickname.val && email.val && first_name.val && last_name.val && password.length >= 5) {
-    $('#submit').removeAttr('disabled');
-  } else {
-    $('#submit').attr('disabled', 'disabled');
-  }
+jQuery.validator.setDefaults({
+  debug: true,
+  success: "valid"
+});
 
+var form = $( ".form-control" );
+if (form.validate()) {
+  $( "#submit" ).removeAttr( "disabled" );
+} else {
+  $( "#submit" ).attr( "disabled", "disabled" );
 }
