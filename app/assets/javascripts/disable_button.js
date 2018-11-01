af1439db9d6ce27f0ae0785f6b32698d7d42de1e
olindos <yason4ik87@gmail.com>
2018-11-01 17:14:57 +0200

    change disable button

--- app/assets/javascripts/disable_button.js
+++ app/assets/javascripts/disable_button.js
@@ -1,16 +1,8 @@
 $(document).ready(function() {
      $(':input[type="submit"]').prop('disabled', true);
      $('.form-control').keyup(function() {
-        if($(this).val() != '') {
+        if($(this).val().valid) {
            $(':input[type="submit"]').prop('disabled', false);
         }
      });
  });
-/*
-var form1 = $(".form-control");
-  if (form1.valid()) {
-    $("#submit").removeAttr("disabled");
-  } else {
-    $("#submit").attr("disabled", "disabled");
-  }
-*/
